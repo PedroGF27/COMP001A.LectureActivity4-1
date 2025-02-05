0df96e99ad5aff3a07c2ea7b768ee82f2b5ae5c2
Pedro Garcia <pedrogarcia5150@my.whccd.edu>
2025-02-05 12:11:28 -0800

    added code to input grades

--- COMP001A.LectureActivity4-1/Program.cs
+++ COMP001A.LectureActivity4-1/Program.cs
@@ -8,7 +8,31 @@ namespace COMP001A.LectureActivity4_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] grades = new int[5]; // declare array to store 5 grades
+
+            for (int i = 0; i < grades.Length; i++) // collect and store grades
+            {
+                int grade = -1; // Initialize with invalid value to ensure validation starts
+                while (grade < 0 || grade > 100)
+                {
+                    Console.Write($"Enter grade for student {i + 1} (0=100): ");
+                    if (int.TryParse(Console.ReadLine(), out grade)) // Check if input is valid integer
+                    {
+                        if (grade >= 0 && grade <= 100)
+                        {
+                            grades[i] = grade;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Grade must be between 0 and 100.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Inavlid input, please enter a valid number.");
+                        grade = -1; // Reset greade to invalid value to re-enter loop
+                    }
+            }
         }
     }
 }
