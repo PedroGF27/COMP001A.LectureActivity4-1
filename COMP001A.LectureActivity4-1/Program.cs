ba9ad79ba4d6a9efbb08fd7f6d15d0c51fee3673
Pedro Garcia <pedrogarcia5150@my.whccd.edu>
2025-02-05 12:01:08 -0800

    Add project files.

---
+++ COMP001A.LectureActivity4-1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Pedro Garcia
+// COMP003A
+// Johnathan Cruz
+// Calculate average grades
+namespace COMP001A.LectureActivity4_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
